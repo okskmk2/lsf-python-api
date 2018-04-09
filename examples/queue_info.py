f8a04742e84f5d409f67accec609215b6762e0d6
Eunsung Lee <okskmk2@naver.com>
2018-04-09 20:11:29 +0900

    removed semi-column and modify code to python style.

--- examples/queue_info.py
+++ examples/queue_info.py
@@ -2,48 +2,46 @@
 
 from pythonlsf import lsf
 
+
 def query_queue(queue_name):
     """
-    "query queue info"
+    c api style
     """
 
-    if lsf.lsb_init("test") > 0:
-        return -1;
+    if lsf.lsb_init("test") != 0:
+        exit(1)
 
-    intp_num_queues = lsf.new_intp();
-    lsf.intp_assign(intp_num_queues, 1);
-    strArr = lsf.new_stringArray(1);
-    #print lsf.intp_value(intp_num_queues);
-    lsf.stringArray_setitem(strArr, 0, queue_name);
-    #print lsf.stringArray_getitem(strArr, 0);
-    queueInfo = lsf.lsb_queueinfo(strArr,intp_num_queues,None,None,0);
-    if queueInfo != None:
-        print 'queueInfo is not null';
+    intp_num_queues = lsf.new_intp()
+    lsf.intp_assign(intp_num_queues, 1)
+    queue_p = lsf.new_stringArray(1)
+    lsf.stringArray_setitem(queue_p, 0, queue_name)
+    info = lsf.lsb_queueinfo(queue_p, intp_num_queues, None, None, 0)
+    if info is not None:
+        return info.queue, info.description
     else:
         print 'queueInfo is null'
-        return -1;
+        exit(1)
 
-    print 'queue name = %s' % queueInfo.queue;
-    print 'queue description = %s' % queueInfo.description; 
 
-    return 0;
+def queue_info(queue_name=None):
+    """
+    :param queue_name:
+    :return:
+    python api style
+    """
+    if lsf.lsb_init("test") != 0:
+        exit(1)
 
-def printQueueInfo():
-    if lsf.lsb_init("test") > 0:
-        return -1;
+    queue = lsf.new_stringArray(1)
+    lsf.stringArray_setitem(queue, 0, queue_name)
 
-    strArr = lsf.new_stringArray(2); //array length is 2
-    lsf.stringArray_setitem(strArr, 0, "normal");
-    lsf.stringArray_setitem(strArr, 1, "short");
-
-    for info in lsf.get_queue_info_by_name(strArr, 2):
-        print info.queue;
-        print info.description;
+    for info in lsf.get_queue_info_by_name(queue, 0):
+        print info.queue
+        print info.description
         print ''
 
-    return 0;
-    
+
 if __name__ == '__main__':
     print("LSF Clustername is :", lsf.ls_getclustername())
     print(query_queue("normal"))
-    printQueueInfo()
+    queue_info()
